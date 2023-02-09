be6d2840fae6b4d193e1f4ead050c72fc80ccf46
drex007 <vykeoj@gmail.com>
2023-02-09 11:42:12 +0000

    comments

--- starletter/views.py
+++ starletter/views.py
@@ -12,7 +12,7 @@ from django.shortcuts import render
 
 class GenerateEmailOtp(APIView):
     serializer_class = GenerateEmailOtpSerializer
-    //Added comments
+    #Added comments
     def post(self, request, *args, **kwargs):
         data = request.data
         try:
